7dd75167be2f12523e6a42034f99b46ef255eec5
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-05-20 14:01:18 -0700

    style: refine the style of the experiences section

--- components/home/experiences/experience-item.tsx
+++ components/home/experiences/experience-item.tsx
@@ -2,53 +2,52 @@ type Props = {
   organization: string;
   country: string;
   link: string;
-  positions: {
-    title: string;
-    startDate: string;
-    endDate: string;
-    descriptions: string[];
-  }[];
+  title: string;
+  startDate: string;
+  endDate: string;
+  descriptions: string[];
 };
 
 export default function ExperienceItem({
   organization,
   country,
   link,
-  positions,
+  title,
+  startDate,
+  endDate,
+  descriptions,
 }: Props) {
   return (
-    <div className="relative flex flex-col gap-y-2 py-4">
+    <div className="relative flex flex-col py-4">
       <div className="absolute left-3 top-[42px] hidden h-[calc(100%-24px)] w-[2px] -translate-x-1/2 bg-gray-300 sm:block" />
-      <div className="flex items-center justify-center gap-x-2 sm:justify-start">
+      <div className="flex flex-col items-center justify-between gap-x-2 sm:flex-row sm:justify-between">
         {/* <div className="flex size-6 items-center justify-center rounded-full bg-gray-800 opacity-50">
           <div className="size-4 rounded-full border-4 border-gray-200 bg-gray-800" />
         </div> */}
-        <div className="flex size-6 items-center justify-center overflow-hidden rounded-full bg-white">
-          <img
-            src={`https://flagsapi.com/${country}/flat/64.png`}
-            className="min-w-8"
-          />
-        </div>
-
-        <h3 className="text-lg font-semibold">{organization}</h3>
-      </div>
-      <div className="flex flex-col gap-y-2">
-        {positions.map((position, index) => (
-          <div key={index} className="pl-0 sm:pl-[33px]">
-            <div className="flex items-center gap-x-4">
-              <p className="w-full font-semibold text-gray-600">
-                {position.title}
-              </p>
-            </div>
-            <ul className="list-disc pl-6">
-              {position.descriptions.map((description, index) => (
-                <li key={index} className="text-left text-gray-600">
-                  {description}
-                </li>
-              ))}
-            </ul>
+        <div className="flex items-center gap-x-2">
+          <div className="flex size-6 items-center justify-center overflow-hidden rounded-full bg-white">
+            <img
+              src={`https://flagsapi.com/${country}/flat/64.png`}
+              className="min-w-8"
+            />
           </div>
-        ))}
+          <h3 className="text-lg font-semibold">{title}</h3>
+        </div>
+        <p>
+          {startDate} - {endDate}
+        </p>
+      </div>
+      <div className="flex flex-col gap-y-2 pl-0 sm:pl-[33px]">
+        <div className="flex items-center gap-x-4">
+          <p className="w-full font-semibold text-gray-600">{organization}</p>
+        </div>
+        <ul className="list-disc pl-6">
+          {descriptions.map((description, index) => (
+            <li key={index} className="text-left text-gray-600">
+              {description}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- components/home/experiences/experiences-section.tsx
+++ components/home/experiences/experiences-section.tsx
@@ -5,8 +5,8 @@ export default function ExperiencesSection() {
   return (
     <div className="z-10 flex justify-center bg-gray-100 py-12">
       <div className="flex w-full max-w-7xl flex-col items-start gap-y-16 p-4 text-gray-800">
-        <div className="flex flex-col gap-y-2">
-          <h2 className="w-full text-4xl font-semibold">Work Experience</h2>
+        <div className="w-full flex flex-col gap-y-2">
+          <h2 className="text-4xl font-semibold">Work Experience</h2>
           <div className="flex w-full flex-col">
             {workExperiences.map((workExperience, index) => (
               <ExperienceItem
@@ -14,12 +14,15 @@ export default function ExperiencesSection() {
                 organization={workExperience.organization}
                 country={workExperience.country}
                 link={workExperience.link}
-                positions={workExperience.positions}
+                title={workExperience.title}
+                startDate={workExperience.startDate}
+                endDate={workExperience.endDate}
+                descriptions={workExperience.descriptions}
               />
             ))}
           </div>
         </div>
-        <div className="flex flex-col gap-y-2">
+        <div className="w-full flex flex-col gap-y-2">
           <h2 className="w-full text-4xl font-semibold">
             Education Experience
           </h2>
@@ -30,7 +33,10 @@ export default function ExperiencesSection() {
                 organization={educationExperience.organization}
                 country={educationExperience.country}
                 link={educationExperience.link}
-                positions={educationExperience.positions}
+                title={educationExperience.title}
+                startDate={educationExperience.startDate}
+                endDate={educationExperience.endDate}
+                descriptions={educationExperience.descriptions}
               />
             ))}
           </div>
