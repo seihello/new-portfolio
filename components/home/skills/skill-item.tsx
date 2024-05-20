fbd1132eaf2b859b9cc0c002b45ceb4a0da632ac
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-05-20 15:20:35 -0700

    style: add animation to skill items

--- components/home/skills/skill-item.tsx
+++ components/home/skills/skill-item.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function SkillItem({ title, img }: Props) {
   return (
-    <div className="relative flex size-24 justify-center rounded-xl bg-white p-4">
+    <div className="relative flex size-24 justify-center rounded-xl bg-white p-4 hover:animate-pulse">
       <img src={`/img/skill_logo/${img}.svg`} />
       <p className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 whitespace-nowrap rounded-full bg-black px-2 py-[2px] text-xs text-white">
         {title}
