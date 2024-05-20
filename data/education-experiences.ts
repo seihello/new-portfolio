4aef157caf8dd4d5049f9099256f5ae175c87cd4
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-05-20 13:32:14 -0700

    feat: add educational experience

---
+++ data/education-experiences.ts
@@ -0,0 +1,30 @@
+const educationExperiences = [
+  {
+    organization: "Cornerstone College",
+    country: "CA",
+    link: "https://ciccc.ca/",
+    positions: [
+      {
+        title: "Diploma of Web Development",
+        startDate: "Jan. 2023",
+        endDate: "Dec. 2023",
+        descriptions: [],
+      },
+    ],
+  },
+  {
+    organization: "Kyoto Sangyo University",
+    country: "JP",
+    link: "https://www.kyoto-su.ac.jp/index-e.html",
+    positions: [
+      {
+        title: "Bachelor of Computer Science and Engineering",
+        startDate: "Mar. 2017",
+        endDate: "Apr. 2013",
+        descriptions: [],
+      },
+    ],
+  },
+];
+
+export default educationExperiences;
