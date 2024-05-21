d51d1e353f5663b4497c142ed77bb9a50148f36e
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-05-20 19:11:40 -0700

    style: use smaller font size for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-sm sm:text-base">
       <body
         className={cn(
           "font-roboto",
