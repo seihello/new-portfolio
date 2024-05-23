ed59d1ad0fd26eebf4a68d61e9b7ed5454181dbd
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-05-23 00:44:38 -0700

    style: improve the position of experience durations

--- components/home/experiences/experience-item.tsx
+++ components/home/experiences/experience-item.tsx
@@ -22,36 +22,37 @@ export default function ExperienceItem({
   isFinal,
 }: Props) {
   return (
-    <div className="relative flex flex-col py-4">
-      <div
-        className={`absolute left-3 top-[42px] hidden h-[calc(100%-24px)] w-[2px] -translate-x-1/2 sm:block ${isFinal ? "bg-gradient-to-b from-gray-300 to-transparent" : "bg-gray-300"}`}
-      />
-      <div className="flex flex-col items-center justify-between gap-x-2 sm:flex-row sm:justify-between">
-        {/* <div className="flex size-6 items-center justify-center rounded-full bg-gray-800 opacity-50">
-          <div className="size-4 rounded-full border-4 border-gray-200 bg-gray-800" />
-        </div> */}
-        <div className="flex items-center gap-x-2">
-          <div className="flex size-6 items-center justify-center overflow-hidden rounded-full bg-white">
-            <img
-              src={`https://flagsapi.com/${country}/flat/64.png`}
-              className="min-w-8"
-            />
+    <div className="relative flex flex-col gap-y-4 py-4 sm:gap-y-2">
+      <div className="flex flex-col gap-y-2 sm:gap-y-0">
+        <div
+          className={`absolute left-3 top-[42px] hidden h-[calc(100%-24px)] w-[2px] -translate-x-1/2 sm:block ${isFinal ? "bg-gradient-to-b from-gray-300 to-transparent" : "bg-gray-300"}`}
+        />
+        <div className="flex flex-col items-center gap-x-2 sm:flex-row">
+          <div className="flex items-center gap-x-2">
+            <div className="flex size-6 items-center justify-center overflow-hidden rounded-full bg-white">
+              <img
+                src={`https://flagsapi.com/${country}/flat/64.png`}
+                className="min-w-8"
+              />
+            </div>
+            <h3 className="whitespace-nowrap text-lg font-semibold">{title}</h3>
           </div>
-          <h3 className="whitespace-nowrap text-lg font-semibold">{title}</h3>
-        </div>
-        <p>
-          {startDate} - {endDate}
-        </p>
-      </div>
-      <div className="flex flex-col gap-y-2 pl-0 sm:pl-[33px]">
-        <div className="flex items-center justify-center gap-x-2 sm:justify-start">
-          <p className="font-semibold text-gray-600 sm:font-normal">
-            {organization}
+          <p className="hidden text-gray-600 sm:block">
+            <span className="hidden sm:inline">| </span>
+            {startDate} - {endDate}
           </p>
+        </div>
+        <div className="flex items-center justify-center gap-x-2 pl-0 sm:justify-start sm:pl-[33px]">
+          <p>{organization}</p>
           <a href={link} target="_blank">
             <FaArrowUpRightFromSquare size={12} />
           </a>
         </div>
+        <p className="italic text-gray-600 sm:hidden">
+          {startDate} - {endDate}
+        </p>
+      </div>
+      <div className="flex flex-col gap-y-2 pl-0 sm:pl-[33px]">
         <ul className="list-disc pl-6">
           {descriptions.map((description, index) => (
             <li key={index} className="text-left text-gray-600">
