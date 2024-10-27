0879e34dad769b0db85b0d13bfec2a102ebd0be5
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-10-27 14:57:51 -0700

    feat: support Japanese for the introduction

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ import ContactItem from "@/components/home/contact-item";
 import ExperiencesSection from "@/components/home/experiences/experiences-section";
 import ProjectsSection from "@/components/home/projects/projects-section";
 import SkillsSection from "@/components/home/skills/skills-section";
+import introduction from "@/data/introduction";
 import { useState } from "react";
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export default function Home() {
-  const [isEnglish, setIsEnglish] = useState(false);
+  const [isEnglish, setIsEnglish] = useState(true);
   return (
     <main className="flex flex-col items-stretch justify-between text-center sm:text-left">
       <div className="fixed -z-50 h-screen bg-black object-cover">
@@ -57,24 +58,9 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col gap-y-4">
-            <p>
-              {`Hi, I'm Seisuke, a software engineer/full-stack web developer with a
-                Bachelor's degree in Computer Science and 5+ years of experience in
-                software development. I have been dedicated to team goals with a passion
-                for building efficient, maintainable, and user-friendly systems. This
-                experience has provided me with the opportunity to acquire a wide range
-                of skills and experience, spanning from large-scale embedded
-                applications to web development.`}
-            </p>
-            <p>
-              {`Throughout my ten-year journey in the
-                field of software engineering, I have overcome numerous challenges in
-                both technical and managerial capacities, enhancing my problem-solving
-                skills along the way. With the experience I bring, coupled with my
-                refined problem-solving abilities and steadfast perseverance when
-                encountering difficulties, I'm eager to contribute significantly to your
-                team!`}
-            </p>
+            {isEnglish
+              ? introduction.en.map((item, index) => <p key={index}>{item}</p>)
+              : introduction.jp.map((item, index) => <p key={index}>{item}</p>)}
           </div>
         </div>
       </div>
---
+++ data/introduction.ts
@@ -0,0 +1,12 @@
+const introduction = {
+  en: [
+    "Hi, I'm Seisuke. After majoring in Computer Science and Engineering at university, I have spent the past six years working as a software engineer and full-stack web developer, with experience ranging from large-scale embedded systems to web application development. Throughout my career, I've focused on building efficient, maintainable systems with a user-first approach.",
+    "Along the way, I’ve faced various technical challenges as a developer and tackled many issues as a team leader. By working together with my teammates, we’ve overcome these obstacles through persistence and problem-solving. I am eager to bring this resilience and problem-solving skill to help tackle the challenges your team faces!",
+  ],
+  jp: [
+    "こんにちは、山田成祐と申します。私は大学で情報理工学を専攻後、ソフトウェアエンジニアとフルスタックWebエンジニアとして、大規模な制御システムからWebアプリケーション開発まで、合計6年間で多岐にわたる業務を経験してきました。その中で、効率的で保守性の高く、ユーザを第一に考えたシステムの開発を意識してきました。",
+    "この長い道のりの中で、開発者として技術的な課題にぶつかったり、チームを率いる管理者としても多くの問題に直面してきましたが、仲間と共に試行錯誤して乗り越え、解決してきました。この困難に対する忍耐力と問題解決力をもって、皆様の課題解決に貢献したいと考えています。",
+  ],
+};
+
+export default introduction;
