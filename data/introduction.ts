8b2d2adeec6da2138e8803a1cc029c1f5f9db2ee
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-11-11 18:41:09 -0800

    chore: improve Japanese introduction

--- data/introduction.ts
+++ data/introduction.ts
@@ -4,8 +4,8 @@ const introduction = {
     "Along the way, I’ve faced various technical challenges as a developer and tackled many issues as a team leader. By working together with my teammates, we’ve overcome these obstacles through persistence and problem-solving. I am eager to bring this resilience and problem-solving skill to help tackle the challenges your team faces!",
   ],
   jp: [
-    "こんにちは、山田成祐と申します。私は大学で情報理工学を専攻後、ソフトウェアエンジニアとフルスタックWebエンジニアとして、大規模な制御システムからWebアプリケーション開発まで、合計6年間で多岐にわたる業務を経験してきました。その中で、効率的で保守性の高く、ユーザを第一に考えたシステムの開発を意識してきました。",
-    "この長い道のりの中で、開発者として技術的な課題にぶつかったり、チームを率いる管理者としても多くの問題に直面してきましたが、仲間と共に試行錯誤して乗り越え、解決してきました。この困難に対する忍耐力と問題解決力をもって、皆様の課題解決に貢献したいと考えています。",
+    "ご覧いただきありがとうございます。ソフトウェアエンジニアの山田成祐と申します。私は大学で情報理工学を専攻後、大規模な制御システムからWebアプリケーション開発まで、合計6年間で多岐にわたる業務を経験してきました。その中で、効率的で保守性の高く、ユーザを第一に考えたシステムの開発を意識してきました。",
+    "この経験の中で、開発者として技術的な課題にぶつかったり、チームリーダとしても多様な問題に直面してきましたが、周りのメンバと協力し、試行錯誤して解決してきました。この困難に対する忍耐力と問題解決力をもって、皆さまの課題解決に貢献したいと思います。",
   ],
 };
 
