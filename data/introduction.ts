27dd554dbf795c8a7832158a580d53d22c0e322b
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-02-26 02:12:48 +0900

    chore: update introduction

--- data/introduction.ts
+++ data/introduction.ts
@@ -1,11 +1,11 @@
 const introduction = {
   en: [
-    "Hi, I'm Seisuke. After majoring in Computer Science and Engineering at university, I have spent the past six years working as a software engineer and full-stack web developer, with experience ranging from large-scale embedded systems to web application development. Throughout my career, I've focused on building efficient, maintainable systems with a user-first approach.",
-    "Along the way, I’ve faced various technical challenges as a developer and tackled many issues as a team leader. By working together with my teammates, we’ve overcome these obstacles through persistence and problem-solving. I am eager to bring this resilience and problem-solving skill to help tackle the challenges your team faces!",
+    "Hi, I’m Seisuke, a software engineer with six years of experience in both large-scale embedded systems and full-stack web development. With a background in Computer Science and Engineering, I’ve built efficient and maintainable systems while keeping a strong user-first mindset throughout my career.",
+    "Over the years, I’ve taken on complex technical challenges and led teams through demanding projects. By collaborating closely with my teammates, I've consistently delivered effective solutions through persistence and thoughtful problem-solving. I’m excited to bring this experience and resilience to help your team tackle its challenges and build impactful products.",
   ],
   jp: [
-    "ご覧いただきありがとうございます。ソフトウェアエンジニアの山田成祐と申します。私は大学で情報理工学を専攻後、大規模な制御システムからWebアプリケーション開発まで、合計6年間で多岐にわたる業務を経験してきました。その中で、効率的で保守性の高く、ユーザを第一に考えたシステムの開発を意識してきました。",
-    "この経験の中で、開発者として技術的な課題にぶつかったり、チームリーダとしても多様な問題に直面してきましたが、周りのメンバと協力し、試行錯誤して解決してきました。この困難に対する忍耐力と問題解決力をもって、皆さまの課題解決に貢献したいと思います。",
+    "はじめまして、ソフトウェアエンジニアの山田 成祐です。大学で情報理工学を専攻後、大規模な機械制御システムからWebアプリケーション開発まで、8年の間多岐にわたる業務を経験してきました。その中で、効率的で保守性の高く、ユーザを第一に考えたシステムの開発を意識してきました。",
+    "この経験の中で、開発者として技術的な課題に直面することもあれば、チームリーダーとしてさまざまな問題に向き合うこともありましたが、周囲のメンバーと協力しながら試行錯誤を重ね、解決してきました。こうした困難を乗り越える中で培った忍耐力と問題解決力を活かし、皆さまの課題解決に貢献したいと思います。",
   ],
 };
 
